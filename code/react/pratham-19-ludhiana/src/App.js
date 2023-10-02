67764c326338d9e5acd677486eb75ea976daf0c8
Pratham86 <dhimanpratham86@gmail.com>
2023-10-02 11:18:13 +0530

    My Hello Hacktoberfest Commit

---
+++ code/react/pratham-19-ludhiana/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "image">
+        <img src = "https://blog.javascripttoday.com/images/post/hacktoberfest2023_huc1aac4b58030d2114d790bc0a9a11e54_234619_1400x0_resize_box_3.png" width = "70%" height = "400px" alt = "hacktoberfest"/>
+      </div>
+    
+      <h1>Hello Hacktoberfest 2023 :)</h1>
+    </div>
+  );
+}
+
+export default App;
