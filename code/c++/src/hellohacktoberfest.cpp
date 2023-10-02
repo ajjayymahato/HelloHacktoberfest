20d0b2313ce807901a67275bfe22911fe6260ca3
Pragnya Pradhan <pragnyapradhan2002@gmail.com>
2023-10-02 15:52:47 +0530

    My Hello Hacktoberfest Commit in C++

---
+++ code/c++/src/hellohacktoberfest.cpp
@@ -0,0 +1,9 @@
+write hello world in c++
+//write hello world in c++
+// Path: code/c%2B%2B/src/hellohacktoberfest.cpp    
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello Hacktobefest!!";
+    return 0;
+}
