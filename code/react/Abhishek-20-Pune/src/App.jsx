942cadc469a6f6d7a3b85f4818a514363b6d4aee
Abhishek_Kumar <abhishekkumar958830@gmail.com>
2023-10-03 19:29:55 +0530

    feat/added hello with hacktoberfest 2023

---
+++ code/react/Abhishek-20-Pune/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+     <div>
+      <h1>HelloHacktoberfest 2023 with React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
